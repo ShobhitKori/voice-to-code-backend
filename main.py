8106be8986c910f1e6c1f807fd480f359d426e9b
Shobhit Kori <112234874+ShobhitKori@users.noreply.github.com>
2025-06-09 21:33:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,12 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-  "http://localhost:3000",
-  "http://127.0.0.1:3000",
-]
-
 app.add_middleware(
-  CORSMiddleware,
-  allow_origins=origins,
-  allow_credentials=True,
-  allow_methods=["*"],
-  allow_headers=["*"],
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.post("/generate-code")
